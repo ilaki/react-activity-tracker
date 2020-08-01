dd7f68bed06f274ebf1c4438895c026888b8e098
Ellakkian <ellakkian94@gmail.com>
2020-08-02 00:54:19 +0530

    [:wrench:]minor fix

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('express-favicon');
-
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// app.use(favicon(__dirname + '/build/favicon/ico'));
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname,'build')));
+
 app.get('/ping', (req,res)=> {
     return res.send('pong');
 })
